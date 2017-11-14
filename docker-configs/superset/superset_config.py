b483d26d1515efb5178418fe361d4214b64774e8
David Riordan <dr@daveriordan.com>
2017-11-14 11:06:30 -0500

    attempting to fix superset but nah not really tho

--- docker-configs/superset/superset_config.py
+++ docker-configs/superset/superset_config.py
@@ -2,8 +2,14 @@ import os
 
 from werkzeug.contrib.cache import RedisCache
 
-
+### Getting Environment Variables
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY', '')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'superset_postgres')
+POSTGRES_DB = os.getenv('POSTGRES_DB', '')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '')
+POSTGRES_USER = os.getenv('POSTGRES_USER', '')
+REDIS_HOST = os.getenv('REDIS_HOST', 'superset_redis')
+
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
     'CACHE_DEFAULT_TIMEOUT': 300,
@@ -11,7 +17,7 @@ CACHE_CONFIG = {
     'CACHE_REDIS_HOST': 'superset_redis',
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
-    'CACHE_REDIS_URL': 'redis://superset_redis:6379/1'}
+    'CACHE_REDIS_URL': 'redis://' + REDIS_HOST + ':6379/1'}
 SQLALCHEMY_DATABASE_URI = \
     'postgresql+psycopg2://superset:superset@superset_postgres:5432/superset'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
